cd85a06a45690db5e6b503aaf54afb54857f9c90
Azzal-e <thanusero119@gmail.com>
2025-02-24 21:28:35 +0100

    .

--- cassandra/lector.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM pruebas;
-SELECT * FROM pruebas WHERE dni_medico = '12345678A';
-
-
-
-INSERT INTO pruebas (id, dni_medico,  dni_paciente,  tipo_prueba, fecha, resultado) 
-VALUES (uuid(), '99999999E',  '77777777F', 'Test', toTimestamp(now()), 'Pendiente');
