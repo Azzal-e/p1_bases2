123f4341ed0f810f3a739a79d946fa42302b1f23
Azzal-e <thanusero119@gmail.com>
2025-03-13 17:47:34 +0100

    .

--- PR2/E/RELACIONAL/crearTablas.sql
+++ PR2/E/RELACIONAL/crearTablas.sql
@@ -1,9 +1,21 @@
 /*IMPLEMENTACIÓN EN ORACLE DE MODELO RELACIONAL*/
 
--- Eliminar si existe a base de datos y volverla a crear
-DROP DATABASE IF EXISTS Banco;
-CREATE DATABASE Banco;
-USE Banco;
+/*-- Eliminar si existe a base de datos y volverla a crear
+DROP TABLESPACE IF EXISTS Banco;
+CREATE TABLESPACE Banco
+DATAFILE 'Banco.dbf' SIZE 100M AUTOEXTEND ON;
+ALTER USER admin DEFAULT TABLESPACE Banco;
+-- Añadir permisos a usuario admin, escritor y lector de tablespace Banco
+GRANT CREATE TABLE TO admin;
+GRANT CREATE SEQUENCE TO admin;
+GRANT CREATE PROCEDURE TO admin;
+GRANT CREATE TRIGGER TO admin;
+GRANT CREATE VIEW TO admin;
+
+GRANT SELECT ON Banco TO lector;
+GRANT SELECT, INSERT, UPDATE ON Banco TO escritor;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Banco TO admin;
+*/
 
 -- Eliminar si existen las tablas y volverlas a crear
 DROP TABLE IF EXISTS Operacion CASCADE;
@@ -225,5 +237,167 @@ BEGIN
     END IF;
 END;
 /
+
+
+-- Trigger para verificar que ´la fecha de una operación debe ser posterior a la fecha de
+-- de creación de la cuenta que actua como emisora, y como receptora (si la hay en este caso).
+CREATE OR REPLACE TRIGGER trigger_verificar_fecha_operacion
+BEFORE INSERT ON Operacion
+FOR EACH ROW
+DECLARE
+    v_fecha_creacion_Emisora DATE;
+    v_fecha_creacion_Receptora DATE;
+BEGIN
+    SELECT fechaCreacion INTO v_fecha_creacion_Emisora
+    FROM Cuentas
+    WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaEmisora
+    AND numeroCuenta = :NEW.numeroCuenta_cuentaEmisora;
+
+    IF :NEW.tipoOperacion = 'TRANSFERENCIA' THEN
+        SELECT fechaCreacion INTO v_fecha_creacion_Receptora
+        FROM Cuentas
+        WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaReceptora
+        AND numeroCuenta = :NEW.numeroCuenta_cuentaReceptora;
+    END IF;
+
+    IF :NEW.fechaOperacion <= v_fecha_creacion_Emisora OR :NEW.fechaOperacion <= v_fecha_creacion_Receptora THEN
+        RAISE_APPLICATION_ERROR(-20002, 'La fecha de la operación no puede ser anterior a la fecha de creación de una cuenta.');
+    END IF;
+END;
+/
+
+--Trigger para verificar que la fecha de nacimiento de un cliente es anterior a la fecha de creacion de una cuente
+-- de la que es titular.
+CREATE OR REPLACE TRIGGER trigger_verificar_fecha_nacimiento
+BEFORE INSERT ON Titular
+FOR EACH ROW
+DECLARE
+    v_fecha_creacion_cuenta DATE;
+BEGIN
+    SELECT fechaCreacion INTO v_fecha_creacion_cuenta
+    FROM Cuentas
+    WHERE prefijoIBAN = :NEW.prefijoIBAN
+    AND numeroCuenta = :NEW.numeroCuenta;
+
+    IF :NEW.fechaNacimiento >= v_fecha_creacion_cuenta THEN 
+        RAISE_APPLICATION_ERROR(-20003, 'La fecha de nacimiento del cliente no puede ser posterior a la fecha de creación de la cuenta.');
+    END IF;
+END;
+/
+
+
+
+-- Trigger para calcular el IBAN de una cuenta.
+CREATE OR REPLACE TRIGGER trigger_calcular_IBAN
+BEFORE INSERT OR UPDATE ON Cuentas
+FOR EACH ROW
+BEGIN
+    IF :NEW.IBAN IS NULL THEN
+        :NEW.IBAN := :NEW.prefijoIBAN || :NEW.numeroCuenta;
+    END IF;
+END;
+/
+
+--Trigger para verificar que en una operación, la cuenta emisora no pueda quedar en negativo.
+CREATE OR REPLACE TRIGGER trigger_verificar_saldo_cuenta
+BEFORE INSERT OR UPDATE ON Operacion
+FOR EACH ROW
+DECLARE
+    v_saldo_cuenta_emisora NUMBER(15,2);
+    v_saldo_cuenta_receptora NUMBER(15,2);
+BEGIN  
+    IF :NEW.tipoOperacion = 'RETIRADA' or :NEW.tipoOperacion = 'TRANSFERENCIA' THEN
+        SELECT saldo INTO v_saldo_cuenta_emisora
+        FROM Cuentas
+        WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaEmisora
+        AND numeroCuenta = :NEW.numeroCuenta_cuentaEmisora;
+        IF v_saldo_cuenta_emisora < :NEW.cuantia THEN
+            RAISE_APPLICATION_ERROR(-20008, 'La cuenta emisora no puede quedar en negativo.');
+        END IF;
+    END IF;
+END;
+/
+
+-- Trigger para actualizar el saldo de una cuenta tras una insertar  una operación.
+CREATE OR REPLACE TRIGGER trigger_actualizar_saldo
+AFTER INSERT ON Operacion
+FOR EACH ROW
+BEGIN
+    IF :NEW.tipoOperacion = 'INGRESO' THEN
+        UPDATE Cuentas SET saldo = saldo + :NEW.cuantia WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaEmisora AND numeroCuenta = :NEW.numeroCuenta_cuentaEmisora;
+    END IF;
+    IF :NEW.tipoOperacion = 'RETIRADA'  THEN
+        UPDATE Cuentas SET saldo = saldo - :NEW.cuantia WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaEmisora AND numeroCuenta = :NEW.numeroCuenta_cuentaEmisora;
+    END IF;
+    IF :NEW.tipoOperacion = 'TRANSFERENCIA' THEN
+        UPDATE Cuentas SET saldo = saldo - :NEW.cuantia WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaEmisora AND numeroCuenta = :NEW.numeroCuenta_cuentaEmisora;
+        UPDATE Cuentas SET saldo = saldo + :NEW.cuantia WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaReceptora AND numeroCuenta = :NEW.numeroCuenta_cuentaReceptora;
+    END IF;
+END;
+/
+
+-- Trigger para actualizar el saldo de una cuentra tras actualizar una operacion.
+CREATE OR REPLACE TRIGGER trigger_actualizar_saldo_operacion
+AFTER UPDATE ON Operacion
+FOR EACH ROW
+BEGIN
+    IF :NEW.tipoOperacion = 'INGRESO' THEN
+        UPDATE Cuentas SET saldo = saldo + :NEW.cuantia - :OLD.cuantia WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaEmisora AND numeroCuenta = :NEW.numeroCuenta_cuentaEmisora;
+    END IF;
+    IF :NEW.tipoOperacion = 'RETIRADA' THEN
+        UPDATE Cuentas SET saldo = saldo - :NEW.cuantia + :OLD.cuantia WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaEmisora AND numeroCuenta = :NEW.numeroCuenta_cuentaEmisora;
+    END IF;
+    IF :NEW.tipoOperacion = 'TRANSFERENCIA' THEN
+        UPDATE Cuentas SET saldo = saldo - :NEW.cuantia + :OLD.cuantia WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaEmisora AND numeroCuenta = :NEW.numeroCuenta_cuentaEmisora;
+        UPDATE Cuentas SET saldo = saldo + :NEW.cuantia - :OLD.cuantia WHERE prefijoIBAN = :NEW.prefijoIBAN_cuentaReceptora AND numeroCuenta = :NEW.numeroCuenta_cuentaReceptora;
+    END IF;
+END;
+/
+
+
+-- Trigger para actualizar el saldo de una cuenta tras eliminar una operación.
+CREATE OR REPLACE TRIGGER trigger_actualizar_saldo_operacion_delete
+AFTER DELETE ON Operacion
+FOR EACH ROW
+BEGIN
+    IF :OLD.tipoOperacion = 'INGRESO' THEN
+        UPDATE Cuentas SET saldo = saldo - :OLD.cuantia WHERE prefijoIBAN = :OLD.prefijoIBAN_cuentaEmisora AND numeroCuenta = :OLD.numeroCuenta_cuentaEmisora;
+    END IF;
+    IF :OLD.tipoOperacion = 'RETIRADA' THEN
+        UPDATE Cuentas SET saldo = saldo + :OLD.cuantia WHERE prefijoIBAN = :OLD.prefijoIBAN_cuentaEmisora AND numeroCuenta = :OLD.numeroCuenta_cuentaEmisora;
+    END IF;
+    IF :OLD.tipoOperacion = 'TRANSFERENCIA' THEN
+        UPDATE Cuentas SET saldo = saldo - :OLD.cuantia WHERE prefijoIBAN = :OLD.prefijoIBAN_cuentaEmisora AND numeroCuenta = :OLD.numeroCuenta_cuentaEmisora;
+        UPDATE Cuentas SET saldo = saldo - :OLD.cuantia WHERE prefijoIBAN = :OLD.prefijoIBAN_cuentaReceptora AND numeroCuenta = :OLD.numeroCuenta_cuentaReceptora;
+    END IF;
+END;
+/
+
+-- Crear el procedimiento para actualizar el saldo de las cuentas de ahorro
+CREATE OR REPLACE PROCEDURE actualizar_saldo_ahorro IS
+BEGIN
+    UPDATE Cuentas
+    SET saldo = saldo + (saldo * interes / 100)
+    WHERE esCuentaCorriente = FALSE;
+END;
+/
+
+-- Crear el job para ejecutar el procedimiento cada noche
+BEGIN
+    DBMS_SCHEDULER.create_job (
+        job_name        => 'job_actualizar_saldo_ahorro',
+        job_type        => 'PLSQL_BLOCK',
+        job_action      => 'BEGIN actualizar_saldo_ahorro; END;',
+        start_date      => SYSTIMESTAMP,
+        repeat_interval => 'FREQ=DAILY; BYHOUR=0; BYMINUTE=0; BYSECOND=0',
+        enabled         => TRUE
+    );
+END;
+/
+
+
+
+
+
 
 
