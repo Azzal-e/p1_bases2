9b752d3be9848fa1a05d726dad5415f27c6f6248
Azzal-e <thanusero119@gmail.com>
2025-03-15 09:42:36 +0100

    .

---
+++ PR2/E/db4o/SRC/Cliente.java
@@ -0,0 +1,92 @@
+import java.util.List;
+import java.util.Date;
+
+public class Cliente {
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private String dni;
+    private Date fechaNacimiento;
+    private List<Cuenta> cuentas;
+
+    public Cliente(){
+        this.cuentas = new ArrayList<>();
+    }
+
+    public Cliente(String nombre, String direccion, String telefono, String email, String dni, Date fechaNacimiento){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+        this.cuentas = new ArrayList<>();
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getDireccion(){
+        return this.direccion;
+    }
+
+    public void setDireccion(String direccion){
+        this.direccion = direccion;
+    }
+
+    public String getTelefono(){
+        return this.telefono;
+    }
+
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getDni(){
+        return this.dni;
+    }
+
+    public void setDni(String dni){
+        this.dni = dni;
+    }
+
+    public Date getFechaNacimiento(){
+        return this.fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento){
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    public List<Cuenta> getCuentas(){
+        return this.cuentas;
+    }
+
+    public void addCuenta(Cuenta cuenta){
+        this.cuentas.add(cuenta);
+    }
+
+    public void removeCuenta(Cuenta cuenta){
+        this.cuentas.remove(cuenta);
+    }
+
+    public String toString(){
+        return "Cliente: " + this.nombre + " " + this.dni 
+        + " " + this.fechaNacimiento + " " + this.direccion + " " + this.telefono + " " + this.email + " " + this.cuentas;
+    }
+
+}
